92a4a3a8fc16f62d0d2113e0d88c89a5d2fba6e7
kddez <94012938+kddez@users.noreply.github.com>
2022-03-11 16:49:20 -0300

    First branch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{ apiKey: 'AIzaSyBMajc2BQcIFzdMOoK2yk-bZv4HRtZIE9Q',
+  authDomain: 'myprofile-fca3c.firebaseapp.com',
+  projectId: 'myprofile-fca3c',
+  storageBucket: 'myprofile-fca3c.appspot.com',
+  messagingSenderId: '25160886202',
+  appId: '1:25160886202:web:067f2b8e900bf144735f83'
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{ apiKey: 'AIzaSyBMajc2BQcIFzdMOoK2yk-bZv4HRtZIE9Q',
+  authDomain: 'myprofile-fca3c.firebaseapp.com',
+  projectId: 'myprofile-fca3c',
+  storageBucket: 'myprofile-fca3c.appspot.com',
+  messagingSenderId: '25160886202',
+  appId: '1:25160886202:web:067f2b8e900bf144735f83'
+}
 };
 
 /*
