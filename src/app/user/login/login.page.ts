cb4c583b51b994a8d6e69d72efc3da1987b3223b
kddez <94012938+kddez@users.noreply.github.com>
2022-03-22 16:42:20 -0300

    branch

--- src/app/user/login/login.page.ts
+++ src/app/user/login/login.page.ts
@@ -28,33 +28,35 @@ export class LoginPage implements OnInit {
     // Faz a autenticação do usuário pelo provedor
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
 
-    // Se der certo...
-    .then(
+      // Se der certo...
+      .then(
 
-      // Obtém os dados do usuário
-      (user) => {
-        console.log(user.user);
-        this.presentAlert(user.user.displayName);
-      }
-    );
+        // Obtém os dados do usuário
+        (user) => {
+
+          // "Chama" a caixa de alerta
+          this.presentAlert(user.user.displayName);
+        }
+      );
   }
 
+  // Exibe uma caixa de alerta ao logar-se
+  // Referências: https://ionicframework.com/docs/api/alert
   async presentAlert(userName) {
     const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
       header: `Olá ${userName}!`,
       message: 'Você já pode acessar nosso conteúdo exclusivo...',
       buttons: [
         {
           text: 'Ok',
           handler: () => {
-            // Vai para a página inicial
+
+            // Quando clicar em [Ok] na caixa, vai para a página inicial
             this.route.navigate(['/inicio']);
           }
         }
       ]
     });
-
     await alert.present();
   }
 
--- src/app/user/profile/profile.page.ts
+++ src/app/user/profile/profile.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 // Importa dependências
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -12,13 +11,12 @@ import { Router } from '@angular/router';
 export class ProfilePage implements OnInit {
 
   // Variável que armazena dados do usuário logado
-  user: any;
+  public userData: any;
 
   constructor(
 
     // Injeta dependências
-    public auth: AngularFireAuth,
-    private route: Router
+    public auth: AngularFireAuth
   ) { }
 
   ngOnInit() {
@@ -26,20 +24,10 @@ export class ProfilePage implements OnInit {
     // Verifica se esta logado
     this.auth.authState.subscribe(user => {
       if (user) {
-        this.user = user;
-
-        // Converte as datas do perfil para JavaScript
-        const createdAt = new Date(this.user.metadata.createdAt);
-        const lastLoginAt = new Date(this.user.metadata.lastLoginAt);
+        this.userData = user;
       }
     });
 
-  }
-
-  // Envia usuário para perfil do Google em uma nova aba/janela do navegador
-  toGoogle() {
-    window.open('https://myaccount.google.com/');
-    return false;
   }
 
 }
